52777b96a9d2c7d5d23c9dc401a58d6c097444b1
KristijanPill <57538101+KristijanPill@users.noreply.github.com>
2022-06-04 18:34:06 +0200

    fix: comment body

--- common/proto/pb/article_service.pb.gw.go
+++ common/proto/pb/article_service.pb.gw.go
@@ -362,13 +362,17 @@ func local_request_ArticleService_GetArticleComments_0(ctx context.Context, mars
 
 }
 
-var (
-	filter_ArticleService_CreateArticleComment_0 = &utilities.DoubleArray{Encoding: map[string]int{"comment": 0, "slug": 1}, Base: []int{1, 1, 1, 0}, Check: []int{0, 1, 2, 3}}
-)
-
 func request_ArticleService_CreateArticleComment_0(ctx context.Context, marshaler runtime.Marshaler, client ArticleServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq NewCommentRequest
 	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
 
 	var (
 		val string
@@ -385,13 +389,6 @@ func request_ArticleService_CreateArticleComment_0(ctx context.Context, marshale
 	err = runtime.PopulateFieldFromPath(&protoReq, "comment.slug", val)
 	if err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "comment.slug", err)
-	}
-
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_ArticleService_CreateArticleComment_0); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
 	msg, err := client.CreateArticleComment(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
@@ -403,6 +400,14 @@ func local_request_ArticleService_CreateArticleComment_0(ctx context.Context, ma
 	var protoReq NewCommentRequest
 	var metadata runtime.ServerMetadata
 
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
 	var (
 		val string
 		ok  bool
@@ -418,13 +423,6 @@ func local_request_ArticleService_CreateArticleComment_0(ctx context.Context, ma
 	err = runtime.PopulateFieldFromPath(&protoReq, "comment.slug", val)
 	if err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "comment.slug", err)
-	}
-
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_ArticleService_CreateArticleComment_0); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
 	msg, err := server.CreateArticleComment(ctx, &protoReq)
